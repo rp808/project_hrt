879541083ac4053be28248f39554c524dc862241
rp808 <romipatel808@gmail.com>
2022-11-16 12:17:55 -0600

    completed the login components

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Alert, StyleSheet, Text, View, ScrollView, Image ,TouchableOpacity } from 'react-native';
+import SignIn from './assets/Components/SignIn';
 
-export default function App() {
+
+const App = () => { 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <SignIn/>
+
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    //alignItems: 'center',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+
   },
+
+
 });
+
+export default App;
---
+++ assets/Components/signUp.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const signUp = () => {
+  return (
+    <div>signUp</div>
+  )
+}
+
